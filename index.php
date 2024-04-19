e404863da6f09c9ac2bf85136a37e79107d90e88
unknown <nidhi12dvg@gmail.com>
2024-04-19 12:42:09 +0530

    fixed index.php

--- index.php
+++ index.php
@@ -10,21 +10,21 @@ session_destroy();
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" type="image/png" href="/assest/images/favicon.ico" />
+    <link rel="icon" type="image/png" href="./asset/images/favicon.ico" />
     <link rel="stylesheet" href="home.css" />
     <link
       href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css"
       rel="stylesheet"
     />
-    <title>Smart Fuel Station</title>
+    <title>Automatic Fuel Dispenser System</title>
   </head>
   <body>
     <!-- header section starts here -->
     <header>
       <nav>
         <div class="navleft">
-          <img src="./assest/images/logo.png" alt="Logo Image" />
-          <h1 style="font-family: Lato">Smart Fuel Station</h1>
+          <img src="./asset/images/logo.png" alt="Logo Image" />
+          <h1 style="font-family: Lato">Automatic Fuel Dispenser System</h1>
         </div>
         <div class="navright">
           <a href="#home_section">Home</a>
@@ -44,7 +44,7 @@ session_destroy();
         <button><a href="#reg_section">Register</a></button>
       </div>
       <div class="homeright">
-        <img src="./assest/images/hero_image.webp" alt="Hero Image" />
+        <img src="./asset/images/hero_image.webp" alt="Hero Image" />
       </div>
     </section>
     <!-- home-section ends here -->
@@ -55,20 +55,21 @@ session_destroy();
         <div class="howleft">
           <iframe
             id="ytvdo"
-            src="https://www.youtube.com/embed/Wr6R3nQfPts"
+            src="asset\images\prjct_vedio.mp4"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            width="250" height="200"
           ></iframe>
         </div>
         <div class="howright">
           <h1>How a fuel retail solution works <span id="tag">?</span></h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
-            repellat fugiat explicabo nostrum beatae, ullam modi, neque veniam
-            rem dignissimos cum corrupti quas doloribus, veritatis autem ipsum
-            ducimus sit fuga. Lorem ipsum, dolor sit amet consectetur
-            adipisicing elit. Rem aut tempora suscipit? Maxime numquam incidunt,
-            quae illo odit labore quo!
+            Automatic Fuel Dispenser Systems(AFDS) typically integrates hardware 
+            with software for payment processing, inventory management, and 
+            operational control. When a customer selects fuel type and amount, 
+            the AFD's payment processing securely authorizes the transaction via 
+            pre-paid card. Simultaneously, the fuel management system tracks inventory 
+            levels, forecasts demand, and generates orders to replenish stock as needed.
           </p>
         </div>
       </div>
@@ -98,11 +99,10 @@ session_destroy();
               </div>
             </div>
             <div class="cardinfo">
-              <h3>Prolonged waiting times</h3>
+              <h3>To Create a User-Friendly Website</h3>
               <p>
-                One of the major problems that customers face is that they have
-                to wait in long queues which eats up a lot of their time and
-                makes them frustrated and annoyed.
+                Develop an intuitive and user-friendly website
+                that simplifies the fueling process for customers.
               </p>
             </div>
           </div>
@@ -115,11 +115,10 @@ session_destroy();
               </div>
             </div>
             <div class="cardinfo">
-              <h3>Lack of customer retention strategy</h3>
+              <h3>To Implement RFID Document Verification</h3>
               <p>
-                Customer retention is at a major risk as many customers would
-                avoid coming to fuel stations with long queues. Instead, they
-                would prefer a station with little or no queue.
+                Enhance security and accuracy by
+                integrating RFID technology into the fuel authorization process.
               </p>
             </div>
           </div>
@@ -132,11 +131,10 @@ session_destroy();
               </div>
             </div>
             <div class="cardinfo">
-              <h3>Prolonged waiting times</h3>
+              <h3>To Ensure Secure User Authentication</h3>
               <p>
-                One of the major problems that customers face is that they have
-                to wait in long queues which eats up a lot of their time and
-                makes them frustrated and annoyed.
+                Implement a stringent user authentication
+                process that prioritizes security.
               </p>
             </div>
           </div>
@@ -149,11 +147,11 @@ session_destroy();
               </div>
             </div>
             <div class="cardinfo">
-              <h3>Prolonged waiting times</h3>
+              <h3>To Integrate a Secure Payment Gateway</h3>
               <p>
-                One of the major problems that customers face is that they have
-                to wait in long queues which eats up a lot of their time and
-                makes them frustrated and annoyed.
+                Prioritize the safety and convenience of
+                customers by seamlessly incorporating a robust and 
+                secure online payment gateway.
               </p>
             </div>
           </div>
@@ -166,11 +164,10 @@ session_destroy();
               </div>
             </div>
             <div class="cardinfo">
-              <h3>Prolonged waiting times</h3>
+              <h3>To Establish Real-Time Data Handling</h3>
               <p>
-                One of the major problems that customers face is that they have
-                to wait in long queues which eats up a lot of their time and
-                makes them frustrated and annoyed.
+                Develop a system that excels in real-time
+                communication with RFID tags and vehicle documents.
               </p>
             </div>
           </div>
@@ -183,11 +180,10 @@ session_destroy();
               </div>
             </div>
             <div class="cardinfo">
-              <h3>Prolonged waiting times</h3>
+              <h3>To Control Fuel Dispensing</h3>
               <p>
-                One of the major problems that customers face is that they have
-                to wait in long queues which eats up a lot of their time and
-                makes them frustrated and annoyed.
+                Seamlessly integrate the system with fuel dispensing
+                equipment to implement a foolproof process.
               </p>
             </div>
           </div>
@@ -264,11 +260,12 @@ session_destroy();
     <footer>
       <div>
         <div class="one">
-          <h1>Smart Fuel</h1>
+          <h1>Automatic Fuel Dispenser System</h1>
           <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora
-            aperiam harum obcaecati porro mollitia molestiae, quis saepe enim
-            illum praesentium rerum illo delectus voluptates perferendis.
+            By leveraging RFID technology and automated processes, 
+            AFDS not only enhances operational efficiency for petrol 
+            stations but also improves customer convenience and satisfaction, 
+            paving the way for a modernized and streamlined fuel dispensing experience.
           </p>
         </div>
         <div class="two">
@@ -283,7 +280,7 @@ session_destroy();
         </div>
       </div>
       <div class="lowerfooter">
-        <p>&copy; 2023 Smart Fuel Station</p>
+        <p>&copy; 2024 All Rights Reserved</p>
       </div>
     </footer>
     <!-- Footer ends here -->
